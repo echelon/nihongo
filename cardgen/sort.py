be0265298b62c94ee6887cfc9fc4afaf7520c584
Brandon Thomas <echelon@gmail.com>
2018-12-07 01:01:17 -0500

    Sorting works with new format & produces nice YAML

--- cardgen/sort.py
+++ cardgen/sort.py
@@ -4,6 +4,7 @@
 Sort entries into a well-defined order.
 """
 
+import sys
 import glob
 import toml
 from toml.encoder import TomlEncoder
@@ -11,6 +12,60 @@ from toml.decoder import TomlDecoder
 from collections import OrderedDict
 
 INDEX_NAME = 'cards'
+
+unicode = str
+
+def _dump_str(v):
+  """
+  Function lifted and modified from original python toml library.
+  """
+  if sys.version_info < (3,) and hasattr(v, 'decode') and isinstance(v, str):
+    v = v.decode('utf-8')
+  v = "%r" % v # NB: adds wrapping quotes
+  if v[0] == 'u':
+    v = v[1:]
+  if v.startswith("'") or v.startswith('"'):
+    v = v[1:-1]
+  use_singlequote = "'" not in v
+  if use_singlequote:
+    v = v.replace('\\"', '"')
+    v = v.replace("'", "\\'")
+  else:
+    v = v.replace("\\'", "'")
+    v = v.replace('"', '\\"')
+  v = v.split("\\x")
+  while len(v) > 1:
+    i = -1
+    if not v[0]:
+      v = v[1:]
+    v[0] = v[0].replace("\\\\", "\\")
+    # No, I don't know why != works and == breaks
+    joinx = v[0][i] != "\\"
+    while v[0][:i] and v[0][i] == "\\":
+      joinx = not joinx
+      i -= 1
+    if joinx:
+      joiner = "x"
+    else:
+      joiner = "u00"
+    v = [v[0] + joiner + v[1]] + v[2:]
+  if use_singlequote:
+    return unicode("'" + v[0] + "'")
+  else:
+    return unicode('"' + v[0] + '"')
+
+class CustomTomlEncoder(TomlEncoder):
+  def __init__(self, preserve=True):
+    super().__init__(_dict=dict, preserve=preserve)
+    self.dump_funcs[str] = _dump_str
+    self.dump_funcs[unicode] = _dump_str
+
+  def dump_list(self, v):
+    retval = ''
+    for u in v:
+      retval += "" + unicode(self.dump_value(u)) + ", "
+    retval = retval[:-2]
+    return '[{0}]'.format(retval)
 
 def read_toml(filename):
   # Maintain key ordering in each item
@@ -23,19 +78,24 @@ def sort_notes(note_toml_data):
   notes = []
   for note in note_toml_data[INDEX_NAME]:
     notes.append(note)
-  notes = sorted(notes, key=lambda v: v['kana']) # Sort by kana
+  notes = sorted(notes, key=lambda v: v['kana'].lstrip('～')) # Sort by kana, ignore '～'
   return { INDEX_NAME : notes }
 
 def write_toml(note_toml_data, filename):
   # Use inline tables
-  encoder = TomlEncoder(_dict=dict, preserve=True)
+  encoder = CustomTomlEncoder(preserve=True)
   toml_contents = toml.dumps(note_toml_data, encoder=encoder)
   with open(filename, 'w') as f:
     f.write(toml_contents)
 
 for filename in glob.glob('**/*.toml', recursive=True):
+  if 'cardgen' in filename:
+    continue # XXX: Things here shouldn't be processed for now.
   print('Processing file {0}'.format(filename))
-  note_toml = read_toml(filename)
-  verbs_toml = sort_notes(note_toml)
-  write_toml(verbs_toml, filename)
+  try:
+    note_toml = read_toml(filename)
+    sorted_toml = sort_notes(note_toml)
+    write_toml(sorted_toml, filename)
+  except Exception as e:
+    print(e)
 
